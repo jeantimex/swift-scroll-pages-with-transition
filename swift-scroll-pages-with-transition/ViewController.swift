b503ab3271f7213d00ea8915d6ffaf8ba36dbbc1
jeantimex <jean.timex@gmail.com>
2016-11-09 22:28:04 -0800

    Added page control.

--- swift-scroll-pages-with-transition/ViewController.swift
+++ swift-scroll-pages-with-transition/ViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
     
-    var mainScrollView = UIScrollView()
+    var scrollView = UIScrollView()
     var pageCount: Int = 3
     var pageColors = [0x54ACD2, 0xFBA026, 0x61BD6D]
+    var pageControl = UIPageControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configScrollView()
+        configPageControl()
+    }
+    
+    func configScrollView() {
         let pageWidth = view.frame.width
         let pageHeight = view.frame.height
         
@@ -24,16 +30,39 @@ class ViewController: UIViewController {
             let pageView: UIView = UIView(frame: CGRect(x: pageWidth * CGFloat(i), y: 0, width: pageWidth, height: pageHeight))
             
             pageView.backgroundColor = UIColor.init(hex: pageColors[i])
-            mainScrollView.addSubview(pageView)
+            scrollView.addSubview(pageView)
         }
         
-        mainScrollView.frame = view.frame
-        mainScrollView.contentSize.width = view.frame.width * CGFloat(pageCount)
-        mainScrollView.showsHorizontalScrollIndicator = false
-        mainScrollView.isPagingEnabled = true
+        scrollView.frame = view.frame
+        scrollView.contentSize.width = view.frame.width * CGFloat(pageCount)
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+        scrollView.delegate = self
         
         view.backgroundColor = UIColor.white
-        view.addSubview(mainScrollView)
+        view.addSubview(scrollView)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = round(scrollView.contentOffset.x / scrollView.frame.size.width)
+        pageControl.currentPage = Int(pageNumber)
+    }
+    
+    func configPageControl() {
+        pageControl.numberOfPages = pageCount
+        pageControl.currentPage = 0
+        pageControl.tintColor = UIColor.red
+        pageControl.pageIndicatorTintColor = UIColor.black
+        pageControl.currentPageIndicatorTintColor = UIColor.green
+        pageControl.frame = CGRect(x: 50, y: 300, width: 200, height: 50)
+        pageControl.addTarget(self, action: #selector(self.changePage(sender:)), for: UIControlEvents.valueChanged)
+        
+        view.addSubview(pageControl)
+    }
+    
+    func changePage(sender: AnyObject) -> () {
+        let x = CGFloat(pageControl.currentPage) * scrollView.frame.size.width
+        scrollView.setContentOffset(CGPoint(x: x,y :0), animated: true)
     }
 
 }
